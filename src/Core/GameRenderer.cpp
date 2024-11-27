3a0ea9a7112521d5d146e5695134601cd3365630
Kewei Han <55893673+keweihan@users.noreply.github.com>
2024-11-26 21:28:03 -0500

    Fix renderer+surface

--- src/Core/GameRenderer.cpp
+++ src/Core/GameRenderer.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <memory>
 #include <utility>
+#include <iostream>
 
 using namespace UtilSimpleECS;
 
@@ -23,15 +24,15 @@ void GameRenderer::initGameRenderer()
 	}
 
 	// Create renderer for window
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE); // SDL_RENDERER_ACCELERATED for GPU (not compatible with Mac)
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_SetHintWithPriority(SDL_HINT_RENDER_VSYNC, "0", SDL_HINT_OVERRIDE);
+	SDL_GL_SetSwapInterval(0);
+	if (renderer == NULL) {
+		std::cout << "Warning: Hardware acceleration not available. Falling back to software renderer." << std::endl;
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+	}
 	//renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	gameTexture = SDL_CreateTexture(GameRenderer::renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, GameRenderer::SCREEN_WIDTH, GameRenderer::SCREEN_HEIGHT);
-
-	// Set white window surface
-	screenSurface = SDL_GetWindowSurface(window);
-	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-	SDL_UpdateWindowSurface(window);	
-
 }
 
 int GameRenderer::SCREEN_WIDTH  = 960;
@@ -39,5 +40,4 @@ int GameRenderer::SCREEN_HEIGHT = 540;
 
 SDL_Window* GameRenderer::window			= nullptr;
 SDL_Renderer* GameRenderer::renderer		= nullptr;
-SDL_Surface* GameRenderer::screenSurface	= nullptr;
 SDL_Texture* GameRenderer::gameTexture		= nullptr;
